527d32adc0dd34b6c194fa958537eb8d42e8b1f9
Erick Nagoski <ericknagoski@gmail.com>
2022-12-03 13:17:59 -0300

    alterações layout editar produto

--- src/main/java/Controllers/EditProductController.java
+++ src/main/java/Controllers/EditProductController.java
@@ -1,13 +1,78 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class EditProductController {
+
+    @FXML
+    private TextField txtFornecedor;
+
+    @FXML
+    private Text lblEndereco;
+
+    @FXML
+    private Text lblDescricao;
+
+    @FXML
+    private TextField txtCusto;
+
+    @FXML
+    private TextField txtEndereco;
+
     @FXML
     private Button btnSearch;
 
     @FXML
-    private TextField iptCode;
+    private TextField txtCode;
+
+    @FXML
+    private Text lblFornecedor;
+
+    @FXML
+    private TextField txtDescricao;
+
+    @FXML
+    private Text lblEstoque;
+
+    @FXML
+    private TextField txtEstoque;
+
+    @FXML
+    private Text lblIpi;
+
+    @FXML
+    private Text lblUm;
+
+    @FXML
+    private Text lblPreco;
+
+    @FXML
+    private TextField txtIpi;
+
+    @FXML
+    private Text lblCusto;
+
+    @FXML
+    private TextField txtUm;
+
+    @FXML
+    private Button btnSubmit;
+
+    @FXML
+    private TextField txtPreco;
+
+    @FXML
+    void search(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submit(ActionEvent event) {
+
+    }
 
 }
